da8cbee49f177e6ddad5a5e0bdb5d0140c3e1093
Lucas Huang <lucas.huang@planetart.cn>
2022-03-11 14:17:34 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test1', function()
+{
+    sleep(3);
+    header('Content-Type: text/css');
+    echo 'body {background-color: blue;}';
+});
+
+Route::get('/test', function()
+{
+    return view('test');
+});
+
+use MatthiasMullie\Minify;
+Route::get('/showcss', function()
+{
+    $public_path = public_path();
+    $sourcePath = $public_path.'\css\mobile\header.css';
+    $minifier = new Minify\CSS($sourcePath);
+    // joined in 1 output file
+    $sourcePath2 = $public_path.'/css/mobile/footer.css';
+    $minifier->add($sourcePath2);
+    // or just output the content
+    return $minifier->minify();
+});
